ccad4366921734ae52c40a8e7d100c6af78721cb
RyanGlem <ryanglem@hotmail.com>
2021-01-07 17:53:23 -0500

    color selection

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-var text = "Hello"
+
+var grids = document.getElementsByClassName("grid-item")
+var gridParent = document.getElementsByClassName ("grid-container")
+
+
+Array.from(grids).forEach(v => v.addEventListener('mouseover', function() {
+
+    if (v.style.background === 'red') {
+
+        v.style.background = 'white'
+
+    } else {
+
+        v.style.background = 'red';
+    }
+  }));
