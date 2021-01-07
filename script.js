30622bdab9c1534bb08bcf9df92f4e010dea71a3
Erick Lojano <erickjlojano@gmail.com>
2021-01-07 17:52:59 -0500

    broken remove button

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const removeRow = () => {
-   document.getElementById("test").innerHTML = "Remove Column Clicked"
+   document.getElementById("test").innerHTML = "Remove Row Clicked"
+   console.log("Hello")
+}
+
+const removeColumn = () => {
+   document.getElementById("test1").innerHTML = "Remove Column Clicked"
    console.log("Hello")
 }
