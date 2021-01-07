44a729df82046985047672c40262a3d1c4823a9c
RyanGlem <ryanglem@hotmail.com>
2021-01-07 16:29:25 -0500

    columns logic

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-var text = "Hello"
+
+function addColumn ()   {
+
+    var wrapper = document.getElementsByClassName("wrapper") 
+
+    document.addEventListener ("click", function (e) {
+
+        var columns = wrapper.gridcolumns
+
+        if (e.onclick) {
+
+            columns.start++;
+        }
+
+    
+
+    }
+
+
+}
