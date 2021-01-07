421b50f857df52573a0b8e9e09c41dd025daf973
Karim Nekzad <karimnekzad00@gmail.com>
2021-01-07 17:52:44 -0500

    semi working buttons

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+// const addColumn = () => {
+//     document.getElementById("temp").innerHTML = "Add Column Clicked"
+// }
+
+// const addColumn = () => {
+//     const masterDiv = document.getElementsByClassName("grid-container")
+//     masterDiv[0].style.display = "block"
+//     const div = document.createElement("div")
+//     div.className = "grid-item"
+//     masterDiv[0].appendChild(div)
+// }
+
+// const addRow = () => {
+//     const masterDiv = document.getElementsByClassName("grid-container")
+//     masterDiv[0].style.display = "flex"
+//     const div = document.createElement("div")
+//     div.className = "grid-item"
+//     masterDiv[0].appendChild(div)
+// }
+
 const addColumn = () => {
-    document.getElementById("temp").innerHTML = "Add Column Clicked"
+    const masterDiv = document.getElementsByClassName("grid-container")
+    const div = document.createElement("div")
+    div.className = "grid-item"
+    masterDiv[0].appendChild(div)
+}
+
+const addRow = () => {
+    const masterDiv = document.getElementsByClassName("grid-container")
+    const div = document.createElement("div")
+    div.className = "grid-item"
+    masterDiv[0].appendChild(div)
 }
