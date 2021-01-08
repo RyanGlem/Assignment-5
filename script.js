a274e8f983ef0b88ad369b54151acc7d8afda40b
Karim Nekzad <karimnekzad00@gmail.com>
2021-01-07 23:48:45 -0500

    colors working, need remove colors to finish

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let columns = 0
 let rows = 0
 const mainTable = document.getElementById("main-table")
+let masterColor = "purple"
+let color = false
 
 const addColumn = () => {
     columns += 1
 
     const rows = document.getElementsByClassName("row")
 
-    for (let i=0; i<rows.length+1; i++) {
-        const data = document.createElement("td")
-        rows[i].appendChild(data);
+    for (let i = 0; i < rows.length; i++) {
+        rows[i].appendChild(initializer());
     }
 }
 
@@ -22,34 +23,58 @@ const addRow = () => {
 
     mainTable.appendChild(row)
 
-    for (let i=0; i<columns+1; i++) {
-        const data = document.createElement("td")
-        row.appendChild(data)
+    for (let i = 0; i < columns; i++) {
+        row.appendChild(initializer())
     }
 }
 
-
-const removeColumn = () => {
-    if(columns === 0){
-      while(rows > -1){
-        removeRow();
-      }
-    }
-    columns--
-    var columns = mainTable.rows[0].cells.length-1
-    for (let i = 0; i < mainTable.rows.length; i++){
-    mainTable.rows[i].deleteCell(columns);
-  }
-}
-
-
-const removeRow = () => {
-    if (rows === 0){
+let removeRow = () => {
+    if (rows === 1){
         while (columns > -1){
-            removeColumn();
+            removeColumn()
         }
     }
-    rows --;
-    let lastR = mainTable.rows.length - 1;
-    mainTable.deleteRow(lastR);
+    rows -= 1
+    finalRow = mainTable.rows.length - 1
+    mainTable.deleteRow(finalRow)
+}
+
+let removeColumn = () => {
+    if (columns === 1){
+        while (row > -1){
+            removeRow()
+        }
+    }
+    columns -= 1
+    let everyCols = mainTable.rows[0].cells.length - 1
+    for (let i = 0; i < mainTable.rows.length; i++){
+        mainTable.rows[i].deleteCell(everyCols)
+    }
+}
+
+const initializer = () => {
+    const gridCell = document.createElement("td");
+    gridCell.classList.add("gridCell");
+    gridCell.classList.add("unfilled");
+    gridCell.addEventListener("mousedown", e => color = true);
+    gridCell.addEventListener("mousemove", e => {
+        if (color) {
+            gridCell.style.backgroundColor = masterColor;
+            gridCell.classList.remove("unfilled");
+        }
+    });
+    gridCell.addEventListener("mouseup", function(event) {
+        if (color) {
+            color = false;
+        }
+    })
+    return gridCell;
+}
+
+function changeBlue () {
+    masterColor = "blue"
+}
+
+function changeGreen () {
+    masterColor = "green"
 }
