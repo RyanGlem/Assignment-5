4fe81e6d1bd947d720e792ebb41f1a388a4c2f40
Erick Lojano <erickjlojano@gmail.com>
2021-01-07 23:00:31 -0500

    semi functional remove functions

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const addColumn = () => {
 
     const rows = document.getElementsByClassName("row")
 
-    for (let i=0; i<rows.length; i++) {
+    for (let i=0; i<rows.length+1; i++) {
         const data = document.createElement("td")
         rows[i].appendChild(data);
     }
@@ -15,15 +15,41 @@ const addColumn = () => {
 
 const addRow = () => {
     rows += 1
-    
+
     const row = document.createElement("tr")
 
     row.classList.add("row")
 
     mainTable.appendChild(row)
 
-    for (let i=0; i<columns; i++) {
+    for (let i=0; i<columns+1; i++) {
         const data = document.createElement("td")
         row.appendChild(data)
     }
+}
+
+
+const removeColumn = () => {
+    if(columns === 0){
+      while(rows > -1){
+        removeRow();
+      }
+    }
+    columns--
+    var columns = mainTable.rows[0].cells.length-1
+    for (let i = 0; i < mainTable.rows.length; i++){
+    mainTable.rows[i].deleteCell(columns);
+  }
+}
+
+
+const removeRow = () => {
+    if (rows === 0){
+        while (columns > -1){
+            removeColumn();
+        }
+    }
+    rows --;
+    let lastR = mainTable.rows.length - 1;
+    mainTable.deleteRow(lastR);
 }
